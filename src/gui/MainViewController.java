2b57a069ecf0bbaa6f904d499c9cca29694335f9
pedroeproess <estudo.pedro.repositorio@outlook.com>
2022-02-01 18:31:10 -0300

    MainViewController created

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,6 +1,33 @@
 package gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+
 public class MainViewController {
+
+    @FXML
+    private MenuItem menuItemSeller;
+
+    @FXML
+    private MenuItem menuItemDepartment;
+
+    @FXML
+    private MenuItem menuItemAbout;
+
+    @FXML
+    public void onMenuItemSellerAction(){
+        System.out.println("onMenuItemSellerAction");
+    }
+
+    @FXML
+    public void onMenuItemDepartmentAction(){
+        System.out.println("onMenuItemDepartmentAction");
+    }
+
+    @FXML
+    public void onMenuItemAboutAction(){
+        System.out.println("onMenuItemAboutAction");
+    }
 
 
 
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,8 +4,10 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
+import java.awt.*;
 import java.io.IOException;
 
 public class Main extends Application {
@@ -15,9 +17,12 @@ public class Main extends Application {
         try {
 
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-            Parent parent = loader.load();
+            ScrollPane scrollPane = loader.load();
 
-            Scene mainScene = new Scene(parent);
+            scrollPane.setFitToHeight(true);
+            scrollPane.setFitToWidth(true);
+
+            Scene mainScene = new Scene(scrollPane);
             primaryStage.setScene(mainScene);
             primaryStage.setTitle("Sample JavaFx application");
             primaryStage.show();
